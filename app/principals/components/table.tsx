ba28de308abd5883b3d268e4058e8758fed40f0c
Mathu <mathu@mate.lk>
2023-12-12 12:22:33 +0530

    Update

--- app/principals/components/table.tsx
+++ app/principals/components/table.tsx
@@ -3,8 +3,9 @@
 import DeleteModel from './deleteModel'
 import {useEffect, useState} from "react";
 
-import { generateClient } from 'aws-amplify/data';
-import { type Schema } from '@/amplify/data/resource';
+import {generateClient} from 'aws-amplify/data';
+import {type Schema} from '@/amplify/data/resource';
+
 const people:any = [
     {
         full_name: 'Lindsay Walton',
@@ -35,7 +36,7 @@ const client = generateClient<Schema>();
 export default function PrincipalsIndex() {
     const [isOpenDeleteModel, setIsOpenDeleteModel] = useState(false)
 
-
+    const profile_url = 'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
     const [principals, setPrincipals] = useState<Schema['Principal'][]>([]);
 
     async function listPrincipals() {
@@ -102,19 +103,19 @@ export default function PrincipalsIndex() {
                                                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
                                                    <div className="flex items-center">
                                                        <div className="h-10 w-10 flex-shrink-0">
-                                                           <img className="h-10 w-10 rounded-full" src={person.profile_url} alt="" />
+                                                           <img className="h-10 w-10 rounded-full" src={person.profile_url ?? profile_url} alt="" />
                                                        </div>
                                                        <div className="ml-4">
-                                                           <div className="font-medium text-gray-900">{person.full_name}</div>
-                                                           <div className="text-gray-500">{person.email}</div>
+                                                           <div className="font-medium text-gray-900">{person.full_name ?? "Null"}</div>
+                                                           <div className="text-gray-500">{person.email ?? "Null"}</div>
                                                        </div>
                                                    </div>
                                                </td>
                                                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                   <div className="text-gray-900">{person.appointed_date}</div>
+                                                   <div className="text-gray-900">{person.appointed_date ?? "Null"}</div>
                                                </td>
                                                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                   <div className="text-gray-900">{person.appointed_date}</div>
+                                                   <div className="text-gray-900">{person.appointed_date ?? "Null"}</div>
                                                </td>
                                                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                 <span className={person.status === 'active' ? 'inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800' : 'inline-flex rounded-full bg-red-100 px-2 text-xs font-semibold leading-5 text-red-800'}>
